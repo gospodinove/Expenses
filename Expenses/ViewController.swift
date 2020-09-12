45c6cff44adbf4b1d00ddd233371cb174b046309
Emanuil Gospodinov <gospodinove@gmail.com>
2020-09-12 19:47:30 +0300

    Initial Commit

---
+++ Expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Emanuil Gospodinov on 12.09.20.
+//  Copyright © 2020 Emanuil Gospodinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
